3c5bceaf05c8bc3de3ec45dfaa58cfdd16223f52
daniel <dstorres2011@hotmail.com>
2016-05-27 05:00:46 -0500

    Carga del Proyecto Inicial 27/05/2016 5:00

---
+++ app/src/main/java/com/example/dstorres/espolmaps/loadActivity.java
@@ -0,0 +1,13 @@
+package com.example.dstorres.espolmaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
