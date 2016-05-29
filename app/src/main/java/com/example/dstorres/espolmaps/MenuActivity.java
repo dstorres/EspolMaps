bd6dffd899058e6f2c7bea1a5deb8d192ed69cae
daniel <dstorres2011@hotmail.com>
2016-05-29 15:31:04 -0500

    Ventana Inicial al ingreso de la APP

---
+++ app/src/main/java/com/example/dstorres/espolmaps/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.example.dstorres.espolmaps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dstorres on 27/05/2016.
+ */
+public class MenuActivity  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
