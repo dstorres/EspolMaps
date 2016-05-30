7796446bad734afca7dee3a0b48ac0b0cc354ec7
daniel <dstorres2011@hotmail.com>
2016-05-29 23:06:53 -0500

    Ventana menu Principal Agrego  un gif y dos botones

--- app/src/main/java/com/example/dstorres/espolmaps/MenuActivity.java
+++ app/src/main/java/com/example/dstorres/espolmaps/MenuActivity.java
@@ -2,16 +2,45 @@ package com.example.dstorres.espolmaps;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.dstorres.espolmaps.load_src.GifRun;
+
 
 /**
  * Created by dstorres on 27/05/2016.
  */
-public class MenuActivity  extends AppCompatActivity{
-
+public class MenuActivity  extends AppCompatActivity {
+    Button ib_menu_b = null;
+    Button ib_menu_l = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        SurfaceView v = (SurfaceView) findViewById(R.id.surfaceView);
+        ib_menu_b = (Button)findViewById(R.id.ButtonUbicacion);
+        ib_menu_l = (Button)findViewById(R.id.imageButtonLista);
+        GifRun w = new GifRun();
+        w.LoadGiff(v,this,R.drawable.one_piece);
+
+        ib_menu_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MenuActivity.this, "Pulsaste el boton de buscar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ib_menu_l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this, "Pulsaste el boton de listas", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dstorres/espolmaps/loadActivity.java
+++ app/src/main/java/com/example/dstorres/espolmaps/loadActivity.java
@@ -1,13 +1,36 @@
 package com.example.dstorres.espolmaps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class loadActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class loadActivity extends AppCompatActivity {
+    private static final long SPLASH_TME=4000 ;
+    private Intent menuActivity= null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                inicio();
+
+            }
+        };
+
+        Timer timer =  new Timer();
+        timer.schedule(task,SPLASH_TME);
+
+    }
+
+    public void inicio(){
+        menuActivity = new Intent(this, MenuActivity.class);
+        finish();
+        startActivity(menuActivity);
     }
 }
